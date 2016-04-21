f2093bbfea9109b8870b7dea984eb39036dbb057
David Tay <admin@extensions-store.com>
2016-04-21 11:33:39 -0400

    Send alert to each user's devices

--- app/code/community/ExtensionsStore/StoreAlerts/Model/Cron.php
+++ app/code/community/ExtensionsStore/StoreAlerts/Model/Cron.php
@@ -16,31 +16,34 @@ class ExtensionsStore_StoreAlerts_Model_Cron
     public function pushAlerts($schedule)
     {
 		$numPushed = 0;
-    	$devices = Mage::getModel('storealerts/device')->getCollection();
     	$push = Mage::getSingleton('storealerts/push');
-    	 
-    	foreach ($devices as $device){
-    		
-    		$device->load($device->getId());
-    		$deviceToken = $device->getDeviceToken();
-    		$accessToken = $device->getAccessToken();
-    		$userId = $device->getUserId();
-    		$adminUser = Mage::getModel('admin/user')->load($userId);
-    		$email = $adminUser->getEmail();
-    		
-    		$alerts = Mage::getModel('storealerts/alert')->getCollection();
-    		$alerts->addFieldToFilter('sent',0);
-    		$alerts->addFieldToFilter('user_id',$userId);
+    	
+    	$alerts = Mage::getModel('storealerts/alert')->getCollection();
+    	$alerts->addFieldToFilter('sent',0);
+    	$alerts->addFieldToFilter('updated_at',array('gteq' => date('Y-m-d')));
+    	
+    	if ($alerts->getSize()>0){
     		
     		foreach ($alerts as $alert){
-    			
-    			$alert->load($alert->getId());
-    			$message = $alert->getMessage();
-    			$sound = $alert->getSound();
     		
-    			$result = $push->push($deviceToken, $accessToken, $email, $message, $sound);
+    			$userId = $alert->getUserId();
+    			$adminUser = Mage::getModel('admin/user')->load($userId);
     			
-    			if ($result){
+    			if ($adminUser->getId() && $adminUser->getIsActive()){
+    				
+    				$email = $adminUser->getEmail();
+    				$message = $alert->getMessage();
+    				$sound = $alert->getSound();
+    				$devices = Mage::getModel('storealerts/device')->getCollection();
+    				$devices->addFieldToFilter('user_id',$userId);
+    				
+    				foreach ($devices as $device){
+    					$deviceToken = $device->getDeviceToken();
+    					$accessToken = $device->getAccessToken();
+    					
+    					$result = $push->push($deviceToken, $accessToken, $email, $message, $sound);
+    				}
+    				
     				$numPushed++;
     				$datetime = date("Y-m-d H:i:s");
     				$alert->setSent(1)->setUpdatedAt($datetime)->save();
